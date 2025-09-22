b2dd3bd10fb80a24e9db4a391d04b96d9fd35172
WinstonLoo <hbwinstonloo@gmail.com>
2025-09-22 17:38:04 +0200

    include code for plot of sine wave

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from signals import create_sine_wave
 
@@ -8,3 +9,9 @@ sine_wave = create_sine_wave(frequency, duration)
 
 print("First 10 samples of the sine wave:")
 print(sine_wave[:10])
+
+plt.plot(sine_wave[:10])
+plt.ylabel("Amplitude")
+plt.xlabel("time")
+plt.show
+plt.savefig("sineplot.png")
