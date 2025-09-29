265c042115943a94d7c007192e33c6586a5f0106
WinstonLoo <hbwinstonloo@gmail.com>
2025-09-29 16:15:16 +0200

    refactor: update sine and square wave functions for improved clarity and structure

--- signals.py
+++ signals.py
@@ -1,5 +1,17 @@
 import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
 
-def create_sine_wave(frequency, duration):
-    t = np.linspace(0, duration, int(44100 * duration))
-    return np.sin(2 * np.pi * frequency * t)
+# Frequency f, start s_t, end e_t, amplitude a, sample rate s_t
+
+def create_sine_wave(f, s_t, e_t, a, s_r):
+    duration = e_t - s_t
+    t = np.linspace(s_t, e_t, int(s_r * duration))
+    return t, a * np.sin(2 * np.pi * f * t)
+
+def create_square_wave(f, s_t, e_t, a, s_r):
+    duration = e_t - s_t
+    t = np.linspace(s_t, e_t, int(s_r * duration))
+    sine_wave = np.sin(2 * np.pi * f * t)
+    return t, a * np.sign(sine_wave)
+
