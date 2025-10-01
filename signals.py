7552d96241aaf55408d27c35370bd0a03a0a589c
WinstonLoo <hbwinstonloo@gmail.com>
2025-10-01 16:52:07 +0200

    refactor: return time array along with signal values in signal creation functions

--- signals.py
+++ signals.py
@@ -8,26 +8,31 @@ import matplotlib.pyplot as plt
 def create_sine_signal(f, s_t, e_t, a, s_r):
     duration = e_t - s_t
     t = np.linspace(s_t, e_t, int(s_r * duration))
-    return a * np.sin(2 * np.pi * f * t)
+    v = a * np.sin(2 * np.pi * f * t)
+    return t,v
 
 def create_square_signal(f, s_t, e_t, a, s_r):
     duration = e_t - s_t
     t = np.linspace(s_t, e_t, int(s_r * duration))
     sine_wave = np.sin(2 * np.pi * f * t)
-    return a * np.sign(sine_wave)
+    v = a * np.sign(sine_wave)
+    return t,v
 
 def create_sawtooth_signal(f, s_t, e_t, a, s_r):
     duration = e_t - s_t
     t = np.linspace(s_t, duration, int(s_r * duration))
-    return a * signal.sawtooth(2 * np.pi * f * t)
+    v = a * signal.sawtooth(2 * np.pi * f * t)
+    return t,v
 
 def create_triangle_signal(f, s_t, e_t, a, s_r):
     duration = e_t - s_t
     t = np.linspace(s_t, e_t, int(s_r * duration))
-    return a * signal.sawtooth(2 * np.pi * f * t, 0.5)
+    v = a * signal.sawtooth(2 * np.pi * f * t, 0.5)
+    return t,v
 
 def create_unit_step_signal(s_t, e_t, s_r, a,step_time):
     duration = e_t - s_t
     t = np.linspace(s_t, e_t, int(s_r * duration))
-    return np.where(t >= step_time, a, 0)
+    v = np.where(t >= step_time, a, 0)
+    return t,v
 
