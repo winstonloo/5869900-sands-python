90c69f94d0a834efb27268addacb50b573a22d61
WinstonLoo <hbwinstonloo@gmail.com>
2025-10-01 16:07:06 +0200

    feat: enhance plotting by using subplots for wave and time-scaled wave visualization

--- run.py
+++ run.py
@@ -12,16 +12,18 @@ s_r=100
 wave = create_triangle_signal(f, s_t, e_t, a, s_r)
 
 time_shifted_wave = time_shift(wave, 2, s_r)
+
 time_scaled_wave = time_scale(wave, 2)
+
 amplitude_scaled_wave = amplitude_scale(wave, 2)
+
 added_wave = add_signals(wave, time_shifted_wave)
+
 multiplied_wave = multiply_signals(wave, time_shifted_wave)
 
-plt.figure()
-plt.plot(added_wave)
-plt.figure()
-plt.plot(wave)
-plt.ylabel("Amplitude")
-plt.xlabel("time")
+fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
+ax1.plot(wave)
+ax2.plot(time_scaled_wave)
+
 plt.show()
-plt.savefig("sineplot.png")
+
