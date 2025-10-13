fa536547a69d49f7622d338058c7467ca01c89e8
WinstonLoo <hbwinstonloo@gmail.com>
2025-10-13 16:18:46 +0200

    docs: add docstrings for signal creation functions

--- signals.py
+++ signals.py
@@ -6,6 +6,29 @@ import matplotlib.pyplot as plt
 # Frequency f, start s_t, end e_t, amplitude a, sample rate s_r
 
 def create_sine_signal(f, s_t, e_t, a, s_r):
+    """
+    Create a sine wave signal.
+    
+    Parameters:
+    -----------
+    f : float
+        Frequency of the sine wave in Hz
+    s_t : float
+        Start time of the signal in seconds
+    e_t : float
+        End time of the signal in seconds
+    a : float
+        Amplitude of the signal
+    s_r : float
+        Sample rate in samples per second
+    
+    Returns:
+    --------
+    t : numpy.ndarray
+        Time array
+    v : numpy.ndarray
+        Signal values array
+    """
     duration = e_t - s_t
     if duration < 0:
         return np.array([]), np.array([])
@@ -14,6 +37,29 @@ def create_sine_signal(f, s_t, e_t, a, s_r):
     return t,v
 
 def create_square_signal(f, s_t, e_t, a, s_r):
+    """
+    Create a square signal.
+    
+    Parameters:
+    -----------
+    f : float
+        Frequency of the sine wave in Hz
+    s_t : float
+        Start time of the signal in seconds
+    e_t : float
+        End time of the signal in seconds
+    a : float
+        Amplitude of the signal
+    s_r : float
+        Sample rate in samples per second
+    
+    Returns:
+    --------
+    t : numpy.ndarray
+        Time array
+    v : numpy.ndarray
+        Signal values array
+    """
     duration = e_t - s_t
     if duration < 0:
         return np.array([]), np.array([])
@@ -23,6 +69,29 @@ def create_square_signal(f, s_t, e_t, a, s_r):
     return t,v
 
 def create_sawtooth_signal(f, s_t, e_t, a, s_r):
+    """
+    Create a wave signal.
+    
+    Parameters:
+    -----------
+    f : float
+        Frequency of the sine wave in Hz
+    s_t : float
+        Start time of the signal in seconds
+    e_t : float
+        End time of the signal in seconds
+    a : float
+        Amplitude of the signal
+    s_r : float
+        Sample rate in samples per second
+    
+    Returns:
+    --------
+    t : numpy.ndarray
+        Time array
+    v : numpy.ndarray
+        Signal values array
+    """
     duration = e_t - s_t
     if duration < 0:
         return np.array([]), np.array([])
@@ -31,6 +100,29 @@ def create_sawtooth_signal(f, s_t, e_t, a, s_r):
     return t,v
 
 def create_triangle_signal(f, s_t, e_t, a, s_r):
+    """
+    Create a triangle wave signal.
+    
+    Parameters:
+    -----------
+    f : float
+        Frequency of the sine wave in Hz
+    s_t : float
+        Start time of the signal in seconds
+    e_t : float
+        End time of the signal in seconds
+    a : float
+        Amplitude of the signal
+    s_r : float
+        Sample rate in samples per second
+    
+    Returns:
+    --------
+    t : numpy.ndarray
+        Time array
+    v : numpy.ndarray
+        Signal values array
+    """
     duration = e_t - s_t
     if duration < 0:
         return np.array([]), np.array([])
@@ -39,6 +131,29 @@ def create_triangle_signal(f, s_t, e_t, a, s_r):
     return t,v
 
 def create_unit_step_signal(s_t, e_t, s_r, a,step_time):
+    """
+    Create a unit step wave signal.
+    
+    Parameters:
+    -----------
+    f : float
+        Frequency of the sine wave in Hz
+    s_t : float
+        Start time of the signal in seconds
+    e_t : float
+        End time of the signal in seconds
+    a : float
+        Amplitude of the signal
+    step_time : float
+        Time at which the step occurs in seconds
+    
+    Returns:
+    --------
+    t : numpy.ndarray
+        Time array
+    v : numpy.ndarray
+        Signal values array
+    """
     duration = e_t - s_t
     if duration < 0:
         return np.array([]), np.array([])
